e198bbf660a13dd6bd08d8c19edea699f30a5de5
Matthew Weber <mtwb47@gmail.com>
2021-09-09 23:17:36 -0400

    Some color changes

--- dwm/config.h
+++ dwm/config.h
@@ -4,23 +4,24 @@
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=9" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#b8bb26";
+static const char col_gray2[]       = "#ebdbb2";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#7AA99F";
+static const char col_gray5[]       = "#8ec07c";
+static const char col_cyan[]        = "#fe8019";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray5  },
 };
 
 typedef struct {
