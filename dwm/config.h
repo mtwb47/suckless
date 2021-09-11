93b4b7095398ad8c0bc5d769ea6ee879dad56f3a
Matthew Weber <mtwb47@gmail.com>
2021-09-11 15:30:13 -0400

    Before adding restart patch

--- dwm/config.h
+++ dwm/config.h
@@ -17,7 +17,7 @@ static const char col_gray2[]       = "#ebdbb2";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray4[]       = "#ffffff";
 static const char col_gray5[]       = "#8ec07c";
-static const char col_cyan[]        = "#fe8019";
+static const char col_cyan[]        = "#d65d0e";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
